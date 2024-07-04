28b3663555d3a81c4a3beec9960a3b73a5de442c
luizfelipp <102230799+luizfelipp@users.noreply.github.com>
2024-07-04 11:47:48 -0300

    exceção para CPF e e-mail já cadastrado

--- src/main/java/com/valdir/helpdesk/resource/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/valdir/helpdesk/resource/exceptions/ResourceExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.valdir.helpdesk.resource.exceptions;
 
-import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.valdir.helpdesk.service.exceptions.DataIntegrityViolationException;
 import com.valdir.helpdesk.service.exceptions.ObjectNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,8 +14,14 @@ import jakarta.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ResourceExceptionHandler {
     @ExceptionHandler(ObjectNotFoundException.class)
-    public  ResponseEntity<StandardError> ObjectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request){
+    public  ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request){
         StandardError error = new StandardError("Object Not Found", ex.getMessage(), request.getRequestURI(), HttpStatus.NOT_FOUND.value(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-    }  
+    }
+    
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public  ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
+        StandardError error = new StandardError("Violação de dados", ex.getMessage(), request.getRequestURI(), HttpStatus.BAD_REQUEST.value(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 }
--- src/main/java/com/valdir/helpdesk/service/TecnicoService.java
+++ src/main/java/com/valdir/helpdesk/service/TecnicoService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.valdir.helpdesk.domain.Pessoa;
 import com.valdir.helpdesk.domain.Tecnico;
 import com.valdir.helpdesk.domain.dtos.TecnicoDTO;
+import com.valdir.helpdesk.repository.PessoaRepository;
 import com.valdir.helpdesk.repository.TecnicoRepository;
+import com.valdir.helpdesk.service.exceptions.DataIntegrityViolationException;
 import com.valdir.helpdesk.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,7 +19,8 @@ public class TecnicoService {
 
     @Autowired
     private TecnicoRepository tecnicoRepository;
-
+    @Autowired
+    private PessoaRepository pessoaRepository;
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = tecnicoRepository.findById(id);
         return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id:"+id));
@@ -28,7 +32,20 @@ public class TecnicoService {
 
     public Tecnico create(TecnicoDTO tecnicoDTO) {
         tecnicoDTO.setId(null);
+        validaPorCpfEmail(tecnicoDTO);
         Tecnico tecnico = new Tecnico(tecnicoDTO);
         return tecnicoRepository.save(tecnico);
     }
+
+    private void validaPorCpfEmail(TecnicoDTO tecnicoDTO) {
+        Optional<Pessoa>  obj = pessoaRepository.findByCpf(tecnicoDTO.getCpf());
+        if(obj.isPresent() && obj.get().getId() != tecnicoDTO.getId() ){
+            throw new  DataIntegrityViolationException("Cpf já cadastrado no sistema!");
+        }
+        obj = pessoaRepository.findByEmail(tecnicoDTO.getEmail());
+        if (obj.isPresent() && obj.get().getId() != tecnicoDTO.getId()) {
+            throw new  DataIntegrityViolationException("E-email já cadstrado no sistema!");     
+        }
+    }
+
 }
